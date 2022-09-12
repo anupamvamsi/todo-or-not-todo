c11258f7a916dd7da1e646295d9d90692e2fdd57
Anupam Vamsi <anupam.ismav@gmail.com>
2022-09-12 20:38:55 +0530

    1. On creation of ToDoItem instances, add them to parent ToDoList.children, 2. Update isUniqueListTitle to check if a ToDoList.title already exists, 3. Update createToDoList to use isAListTitle, 4. Add getter functions to get all ToDoList titles and to also get ToDoList title by given title, 5. Add functions to help add created ToDoItem instances to their tagged ToDoList instances

--- src/App.js
+++ src/App.js
@@ -7,6 +7,44 @@ export default class App {
 
   constructor() {}
 
+  getToDoList(title) {
+    let returnList;
+
+    App.todoLists.forEach((list) => {
+      if (list.title === title) {
+        returnList = list;
+      } else {
+        returnList = undefined;
+      }
+    });
+
+    return returnList;
+  }
+
+  getToDoListTitles() {
+    let listOfTitles = [];
+
+    App.todoLists.forEach((list) => {
+      listOfTitles.push(list.title);
+    });
+
+    return listOfTitles;
+  }
+
+  addChildToList(todoListTitle, todoItem) {
+    const todoList = this.getToDoList(todoListTitle);
+    todoList.children.push(todoItem);
+  }
+
+  addItemToParentList(todoItem) {
+    // if parentList is NOT an existing ToDoList, create ToDoList:
+    if (!this.isAListTitle(todoItem.parentList)) {
+      this.createToDoList(todoItem.parentList, '');
+    }
+
+    this.addChildToList(todoItem.parentList, todoItem);
+  }
+
   createToDoItem(title, description, dueDate, priority, isDone, parentList) {
     const newItem = new ToDoItem(
       title,
@@ -18,30 +56,34 @@ export default class App {
     );
 
     App.todoItems.push(newItem);
-    // console.log('Todo items:', App.todoItems);
-    // console.log('Todo lists:', App.todoLists);
+
+    // Add todoItem to todoList
+    this.addItemToParentList(newItem);
 
     return newItem;
   }
 
-  isUniqueListTitle(title) {
-    App.todoLists.forEach((list) => {
-      if (list.title === title) {
-        return false;
-      }
-    });
+  isAListTitle(title) {
+    const listOfTitles = this.getToDoListTitles();
 
-    return true;
+    if (listOfTitles.includes(title)) {
+      return true;
+    }
+
+    console.log(`${title} doesn't exist!`);
+    return false;
   }
 
   createToDoList(title, description) {
     let newList;
-    if (this.isUniqueListTitle(title)) {
+    if (!this.isAListTitle(title)) {
+      console.log(`Creating ${title} ToDoList...`);
       newList = new ToDoList(title, description);
+      App.todoLists.push(newList);
+      console.log('Todo lists:', App.todoLists);
+    } else {
+      console.log(`${title} already exists`);
     }
-
-    App.todoLists.push(newList);
-    // console.log('Todo lists:', App.todoLists);
 
     return newList;
   }
