c44fdb317543a7c959e1b4c2a99fb18b6036584a
Anupam Vamsi <anupam.ismav@gmail.com>
2022-09-13 20:47:31 +0530

    1. Add function to get number of items (span element) present in each todoList represented on the sidebar, 2. Add function to get 'Home' todoList element on the sidebar, 3. Add function to get all sidebar todoList elements, 4. Add function to get TEXT CONTENT of all sidebar todoList elements, 5. Add function to get a SPECIFIC sidebar element, including 'Home' todoList sidebar element

--- src/DOMElements.js
+++ src/DOMElements.js
@@ -4,4 +4,47 @@ export default class DOMElements {
   static getElementOfSelector(selector) {
     return document.querySelector(selector);
   }
+
+  static getNumItemsElement(todoList) {
+    if (todoList.title === 'Home') {
+      return DOMElements.getSidebarHomeList().firstElementChild;
+    }
+
+    return DOMElements.getSidebarListElement(todoList).firstElementChild;
+  }
+
+  static getSidebarHomeList() {
+    return document.querySelector('.sidebar-items>li>a');
+  }
+
+  static getSidebarListElements() {
+    const todoListItems = document.querySelectorAll('.sidebar-todo-lists>li');
+
+    return todoListItems;
+  }
+
+  static getSidebarListsContent() {
+    const todoListItems = DOMElements.getSidebarListElements();
+    const todoListContent = [];
+    todoListItems.forEach((item) => todoListContent.push(item.textContent));
+
+    return todoListContent;
+  }
+
+  static getSidebarListElement(todoList) {
+    if (todoList.title === 'Home') {
+      return this.getSidebarHomeList();
+    }
+
+    const todoListItems = DOMElements.getSidebarListElements();
+
+    let retVal;
+    todoListItems.forEach((item) => {
+      if (item.firstChild.textContent === todoList.title) {
+        retVal = item;
+      }
+    });
+
+    return retVal;
+  }
 }
