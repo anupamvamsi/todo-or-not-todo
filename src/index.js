0d74a8a75a3fc7c7d2303e12d6561e4ab2b08e05
Anupam Vamsi <anupam.ismav@gmail.com>
2022-09-25 13:30:06 +0530

    1. Update main() to detect and use localStorage

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ require('./styles/sidebar.css');
 
 import Display from './Display';
 import App from './App';
+import MyStorage from './Storage';
 
-function main() {
+function loadDefaults() {
   App.createToDoList('Home', 'A list of all your tasks');
   App.createToDoList('Chores', 'A list of all your tasks');
   App.createToDoList('Scooby', 'A list of all your tasks');
@@ -19,6 +20,22 @@ function main() {
 
   App.createToDoItem('Second', 'blah blah', '2020-04-23', '4', 'Chores', false);
   App.createToDoItem('Deuxieme', 'blah ', '2020-04-29', '3', 'Home', true);
+}
+
+function main() {
+  if (MyStorage.checkStorageAvailable('localStorage')) {
+    if (
+      !localStorage.getItem('todoItems') &&
+      !localStorage.getItem('todoLists')
+    ) {
+      loadDefaults();
+      App.updateStorage();
+    } else {
+      App.getFromStorage();
+    }
+  } else {
+    loadDefaults();
+  }
 
   Display.displayToDoList(App.getToDoList('Home'), App.todoItems);
 
